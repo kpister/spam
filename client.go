30f7c62c58346d9931d209f9bce3ecc40916d9b6
Kaiser Pister <pisterk@gmail.com>
2017-08-02 11:02:24 -0700

    working stuff

--- client.go
+++ client.go
@@ -4,14 +4,16 @@ import "net"
 import "fmt"
 import "bufio"
 import "os"
+import "reflect"
 
 func main() {
 
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println(reflect.TypeOf(reader))
     // connect to this socket
     conn, _ := net.Dial("tcp", "127.0.0.1:8080")
     for {
         // read in input from stdin
-        reader := bufio.NewReader(os.Stdin)
         fmt.Print("Text to send: ")
         text, _ := reader.ReadString('\n')
         // send to socket
---
+++ clientserver.go
@@ -0,0 +1,84 @@
+package main
+
+// Source: http://divan.github.io/posts/go_concurrency_visualize/
+
+import (
+    "fmt"
+    "net"
+    "bufio"
+    "os"
+)
+
+func handler(c net.Conn, ch chan string) {
+    ch <- c.RemoteAddr().String()
+    for {
+        message, _ := bufio.NewReader(c).ReadString('\n')
+        if message == "" {
+            break
+        }
+        // output message received
+        fmt.Print("Message Received:", string(message))
+        // send new string back to client
+        c.Write([]byte(message + "\n"))
+
+    }
+    c.Close()
+}
+
+func logger(ch chan string) {
+    for {
+        fmt.Println(<-ch)
+    }
+}
+
+func server(l net.Listener, ch chan string) {
+    for {
+        c, err := l.Accept()
+        if err != nil {
+            continue
+        }
+        go handler(c, ch)
+    }
+}
+
+func beclient(reader *bufio.Reader) {
+    var connections []net.Conn
+    for {
+        fmt.Print("Enter command: ")
+        cmd, _ := reader.ReadString('\n')
+        cmd = cmd[:len(cmd)-1]
+        if cmd == "connect" {
+            fmt.Print("Enter address: ")
+            conn, _ := reader.ReadString('\n')
+            conn =  conn[:len(conn)-1]
+            c, _ := net.Dial("tcp", conn)
+            connections = append(connections, c)
+        }
+        if cmd == "broadcast" {
+            if len(connections) == 0 {
+                fmt.Println("You have no connections")
+                continue
+            }
+            fmt.Print("Enter message: ")
+            text, _ := reader.ReadString('\n')
+            for _, v := range connections {
+                fmt.Fprintf(v, text + "\n")
+            }
+        }
+    }
+}
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter port: ")
+    port, _ := reader.ReadString('\n')
+    l, err := net.Listen("tcp", ":" + port[0: len(port)-1])
+    if err != nil {
+        panic(err)
+    }
+    ch := make(chan string)
+    go logger(ch)
+    go server(l, ch)
+    for {
+        beclient(reader)
+    }
+}
