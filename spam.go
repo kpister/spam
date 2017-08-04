bfaeb4400a8ed865f880f7cbd3d677689be177c7
Kaiser Pister <pisterk@gmail.com>
2017-08-04 14:20:41 -0700

    allow for graceful exit in the future

--- spam.go
+++ spam.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+    "os"
+    "os/signal"
+    "fmt"
     "github.com/kpister/spam/spamcore"
     "github.com/kpister/spam/parsecfg"
 )
 
+func handleexit(exit chan os.Signal) {
+    for signal := range exit {
+        // Handle exit status
+        fmt.Println(signal)
+        os.Exit(1)
+    }
+}
+
 func main(){
+    exitchannel := make(chan os.Signal, 1)
+    signal.Notify(exitchannel, os.Interrupt)
+    go handleexit(exitchannel)
+
     cfg := parsecfg.ParseCfg("spam_core.cfg")
 
     spamcore.StartServer(&cfg)
